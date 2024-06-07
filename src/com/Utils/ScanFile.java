cdee9fdb609c7a05235def127603146dc0029ed4
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2024-06-07 09:19:52 +0300

    last changes

--- src/com/Utils/ScanFile.java
+++ src/com/Utils/ScanFile.java
@@ -3,6 +3,7 @@ package com.Utils;
 import com.Annotation.Get;
 import com.Annotation.ToController;
 import com.Mapping.Mapping;
+import jakarta.servlet.ServletException;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -12,33 +13,48 @@ import java.util.HashMap;
 
 public class ScanFile {
 
-    public static void scanner(String packageName, HashMap analise){
+    public static void scanner(String packageName, HashMap analise) throws ServletException {
+        File src=null;
         try {
+            if (packageName.isEmpty()){
+                throw new ServletException("Controller Introuvable");
+            }
             URL url = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "/"));
             // Convertir l'URL en chemin de fichier
-            File src = new File(URLDecoder.decode(url.getFile(), "UTF-8"));
+            src = new File(URLDecoder.decode(url.getFile(), "UTF-8"));
+
             // Lire tous les fichiers dans le répertoire
             for (File p : src.listFiles()) {
                 if (p.getName().endsWith(".class")) {
                     String className = p.getName().replace(".class", "");
-                    // Charger la classe
-                    Class<?> clazz = Class.forName(packageName + "." + className);
-                    // Vérifier si la classe a l'annotation
-                    if (clazz.isAnnotationPresent(ToController.class)) {
-                        Method[] methods = clazz.getMethods();
-                        // Parcourir les méthodes et vérifier si elles ont l'annotation @Get
-                        for (Method method : methods) {
-                            if (method.isAnnotationPresent(Get.class)) {
-                                analise.put(method.getAnnotation(Get.class).url(),new Mapping(packageName+"."+className,method.getName()));
+                    try{
+                        // Charger la classe
+                        Class<?> clazz = Class.forName(packageName + "." + className);
+                        // Vérifier si la classe a l'annotation
+                        if (clazz.isAnnotationPresent(ToController.class)) {
+                            Method[] methods = clazz.getMethods();
+                            // Parcourir les méthodes et vérifier si elles ont l'annotation @Get
+                            for (Method method : methods) {
+                                if (method.isAnnotationPresent(Get.class)) {
+                                    System.out.println(method.getAnnotation(Get.class).url());
+                                    if (!analise.containsKey(method.getAnnotation(Get.class).url())){
+                                        analise.put(method.getAnnotation(Get.class).url(),new Mapping(packageName+"."+className,method.getName()));
+                                    }else {
+                                        throw new ServletException("UrlDoublant");
+                                    }
+                                }
                             }
+                        } else {
+    //                        out.println("tsy izy");
                         }
-                    } else {
-//                        out.println("tsy izy");
+                    }catch (ClassNotFoundException e){
+                        continue;
                     }
+
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new ServletException(e.getMessage());
         }
     }
 }
