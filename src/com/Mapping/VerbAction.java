731a22e6f8ad7016b74d1656bcb5a713cb15e675
AndyRdn <153283134+AndyRdn@users.noreply.github.com>
2024-10-13 22:42:34 +0300

    set_and_error

---
+++ src/com/Mapping/VerbAction.java
@@ -0,0 +1,28 @@
+package com.Mapping;
+
+public class VerbAction {
+    String action;
+    String verb;
+
+    public VerbAction(String action, String verb) {
+        this.action = action;
+        this.verb = verb;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getVerb() {
+        return verb;
+    }
+
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
+
+}
